784a8d1558e4d57ce2b0d043a29c9146a2e0ffdc
Kamil Sajdok <kamil.sajdok@segwarez.com>
2025-03-09 21:48:08 +0100

    Pagination in RequestScope

---
+++ web-frameworks/spring-web/src/main/java/com/segwarez/springweb/infrastructure/configuration/Pagination.java
@@ -0,0 +1,13 @@
+package com.segwarez.springweb.infrastructure.configuration;
+
+import lombok.Data;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+
+@Data
+@Component
+@RequestScope
+public class Pagination {
+    private Pageable pageable;
+}
