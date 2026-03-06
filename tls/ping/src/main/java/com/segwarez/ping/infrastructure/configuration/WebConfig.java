c170228560396d067b249d7cefff369094214248
Kamil Sajdok <kamil.sajdok@segwarez.com>
2026-03-07 00:02:00 +0100

    Spring 4 RestClient fix

--- tls/ping/src/main/java/com/segwarez/ping/infrastructure/configuration/WebConfig.java
+++ tls/ping/src/main/java/com/segwarez/ping/infrastructure/configuration/WebConfig.java
@@ -1,16 +1,23 @@
 package com.segwarez.ping.infrastructure.configuration;
 
-import org.springframework.boot.autoconfigure.web.client.RestClientSsl;
+import org.springframework.boot.ssl.SslBundles;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
+
+import java.net.http.HttpClient;
 
 @Configuration
 public class WebConfig {
     @Bean
-    RestClient restClient(RestClient.Builder builder, RestClientSsl ssl) {
-        return builder
-                .apply(ssl.fromBundle("ping-bundle"))
+    RestClient restClient(SslBundles sslBundles) {
+        var bundle = sslBundles.getBundle("ping-bundle");
+        var httpClient = HttpClient.newBuilder()
+                .sslContext(bundle.createSslContext())
+                .build();
+        return RestClient.builder()
+                .requestFactory(new JdkClientHttpRequestFactory(httpClient))
                 .build();
     }
 }
